3ac2a5ca169daa064c530ef4928c95cdaef6305d
Ediberto Ponce <ediberto01@gmail.com>
2022-04-06 21:04:40 -0700

    Established a good starting point. Form needs some more work

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,80 +4,51 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <header className='flex items-center justify-between p-3 bg-green-500'>
+        <h1 className='text-2xl font-semibold'>Cookie Stand Admin</h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+      </header>
+
+      {/* <main className="flex flex-col items-center justify-center">
+        <form className='flex w-1/2 my-4 bg-green-400'>
+          <input name='location' type="text" placeholder='Enter location' className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border border-gray-200 rounded text-gray-base'></input>
+          <button className='px-2 py-1 bg-green-500'>Create</button>
+        </form>
+      </main> */}
+
+      <div className="flex flex-col items-center justify-center py-5">
+        <p className="mb-3 text-xl font-medium">
+          Create Cookie Stand
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <form className=''>
+          {/* <label className="mb-2 text-lg font-bold uppercase text-grey-darkest" htmlFor="Location"></label> */}
+          <input name='Location'
+            type="text" placeholder="Enter a Location"
+            className="w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border border-gray-200 rounded text-gray-base" />
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            <div>
+            <input aria-label="Enter your password"
+            type="password" placeholder="Password"
+            className="w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border border-gray-200 rounded text-gray-base" />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+          <button type="submit"
+            className="w-full mt-4 bg-green-400">
+            Login
+          </button>
+            </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        </form>
+      </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex items-center justify-between p-3 mt-8 bg-green-500">
+        <h1 className='text-xs'>&copy;2021</h1>
       </footer>
     </div>
   )
