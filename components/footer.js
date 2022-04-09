3cae6d147688f9bf970cc92b6ce48fa6918cab2c
Ediberto Ponce <ediberto01@gmail.com>
2022-04-08 21:04:10 -0700

    Did not need to pass props, got rid of it.

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,4 @@
-export default function Footer(props) {
+export default function Footer() {
   return (
     <footer className="flex items-center justify-between p-3 mt-8 bg-green-500">
       <h1 className='text-xs'>&copy;2021</h1>
