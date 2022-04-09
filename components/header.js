69faf4e7e68325e68e39f83c930ebbb88fe8300f
Ediberto Ponce <ediberto01@gmail.com>
2022-04-08 21:06:08 -0700

    Got rid of props

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-export default function Header(props) {
+export default function Header() {
   return (
     <header className='flex items-center justify-between p-3 bg-green-500'>
       <h1 className='text-2xl font-semibold'>Cookie Stand Admin</h1>
