07c33289c601b03228a65a56c24b886d90ecc33e
Ediberto Ponce <ediberto01@gmail.com>
2022-04-08 21:07:21 -0700

    Renders object to user, fully synced with form

--- components/table.js
+++ components/table.js
@@ -1,8 +1,8 @@
-export default function Table(props) {
+export default function Table({ cookieStand }) {
   return (
-    <section>
-      <p className='text-center'>Report Table Coming Soon...</p>
-      <p className='text-center'>JSON DATA HERE</p>
+    <section className="text-center">
+      <p>Report Table Coming Soon...</p>
+      <p>{"{"}&quot;location&quot;:{cookieStand.location},&quot;minCustomers&quot;:{cookieStand.min_cust_per_hour},&quot;maxCustomers&quot;:{cookieStand.max_cust_per_hour},&quot;avgCookies&quot;:{cookieStand.avg_cookies_per_sale}{"}"}</p>
     </section>
   )
 }
